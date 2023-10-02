b4594bc11aec530381cb29b744b1b0cb47bdb158
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-02 09:56:08 +0330

    create userclass

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,75 @@
 package entity;
 
-public class User {
+import base.entity.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class User extends BaseEntity<Integer> {
+    private String firstName;
+    private String lastName;
+
+    private String userName;
+
+    private String password;
+
+    private String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+
+
+
+
+    public User(String firstName, String lastName, String userName, String password, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.address = address;
+
+    }
+
+    public User() {
+    }
 }
