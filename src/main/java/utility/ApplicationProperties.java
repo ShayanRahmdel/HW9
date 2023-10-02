8c736d94bdabe7907f420a425b88c85599a5e1f6
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-02 14:27:44 +0330

    create ApplicationProperties

---
+++ src/main/java/utility/ApplicationProperties.java
@@ -0,0 +1,8 @@
+package utility;
+
+public class ApplicationProperties {
+
+    public static final String URL = "jdbc:postgresql://localhost:5432/hw9";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "123456";
+}
