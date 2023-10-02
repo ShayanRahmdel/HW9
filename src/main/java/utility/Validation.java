a8fc5304fff3d3e3c50a2e50632d3bc820d46502
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-02 14:30:56 +0330

    create Validation and passwordvalid

---
+++ src/main/java/utility/Validation.java
@@ -0,0 +1,11 @@
+package utility;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+    public static Boolean isValidPassword(String password){
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,10}$");
+        return password.matches(pattern.pattern());
+    }
+
+}
