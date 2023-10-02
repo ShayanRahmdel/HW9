eb3aab8ec5d8a6394eaf5c90f973660a4d60018e
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-02 14:29:06 +0330

    create DataSource

---
+++ src/main/java/utility/DataSource.java
@@ -0,0 +1,23 @@
+package utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource {
+
+    private static Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(ApplicationProperties.URL
+                    , ApplicationProperties.USERNAME, ApplicationProperties.PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
