83d360ca3ccb6255615141c22fa0d7d49e8515a9
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-03 11:56:26 +0330

    create ElecRepository

---
+++ src/main/java/repository/ElecRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import base.repository.BaseRepository;
+import entity.Electronic;
+import entity.ElectronicProduct;
+
+public interface ElecRepository extends BaseRepository<Integer, Electronic> {
+}
