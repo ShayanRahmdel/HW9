70122afa88ba58530b1c0e8074c0866a219a1879
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-03 11:57:01 +0330

    create class Electronic

---
+++ src/main/java/entity/Electronic.java
@@ -0,0 +1,66 @@
+package entity;
+
+import base.entity.BaseEntity;
+import repository.ElecRepository;
+
+public class Electronic  extends BaseEntity<Integer> {
+    private ElectronicProduct electronicProduct;
+
+
+    private double prize;
+
+    private int stock;
+
+
+
+    public Electronic(Integer integer, ElectronicProduct electronicProduct,  double prize, int stock) {
+        super(integer);
+        this.electronicProduct = electronicProduct;
+        this.prize = prize;
+        this.stock = stock;
+    }
+
+    public Electronic(Integer integer, double prize, int stock) {
+        super(integer);
+        this.prize = prize;
+        this.stock = stock;
+    }
+
+    public Electronic( double prize, int stock) {
+        this.prize = prize;
+        this.stock = stock;
+    }
+
+    public Electronic(ElectronicProduct electronicProduct, double prize, int stock) {
+        this.electronicProduct = electronicProduct;
+        this.prize = prize;
+        this.stock = stock;
+    }
+    public ElectronicProduct getElectronicProduct() {
+        return electronicProduct;
+    }
+
+    public void setElectronicProduct(ElectronicProduct electronicProduct) {
+        this.electronicProduct = electronicProduct;
+    }
+
+
+    public double getPrize() {
+        return prize;
+    }
+
+    public void setPrize(double prize) {
+        this.prize = prize;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+
+
+}
