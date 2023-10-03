f5639bae04b5c9e11ee63b06dd63d4366a8f90b1
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-03 11:56:06 +0330

    add ElectronicService and electronicRpo

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,14 +1,22 @@
 package utility;
 
+import repository.ElecRepository;
 import repository.UserRepository;
+import repository.impel.ElecRepositoryImpl;
 import repository.impel.UserRepositoryImpl;
+import service.ElectronicService;
 import service.UserService;
+import service.impel.ElectronicServiceImpl;
 import service.impel.UserServiceImpl;
 
 public class ApplicationContext {
     private static final ApplicationContext applicationContext = new ApplicationContext();
     private UserRepository userRepository;
     private UserService userService;
+
+    private ElecRepository elecRepository;
+
+    private ElectronicService electronicService;
 
 
     public ApplicationContext() {
@@ -35,7 +43,23 @@ public class ApplicationContext {
         return userService;
     }
 
+    public ElecRepository getElecRepository() {
+        if (elecRepository == null) {
+            elecRepository = new ElecRepositoryImpl(
+                    DataSource.getConnection()
+            );
+        }
+        return elecRepository;
+    }
 
 
+    public ElectronicService getElectronicService() {
+        if (electronicService == null) {
+            electronicService = new ElectronicServiceImpl(
+                    getElecRepository()
+            );
+        }
+        return electronicService;
+    }
 
 }
