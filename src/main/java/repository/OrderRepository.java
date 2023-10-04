3a638cf2dee18130eff2e714ed9bfcebbb91547d
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 19:49:50 +0330

    add two three methods

--- src/main/java/repository/OrderRepository.java
+++ src/main/java/repository/OrderRepository.java
@@ -3,7 +3,17 @@ package repository;
 import base.repository.BaseRepository;
 import entity.Order;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface OrderRepository extends BaseRepository<Integer, Order> {
 
     double sumPrize();
+
+
+
+    void deleteByProductID(int id) throws SQLException;
+    int findProductID(int id) throws SQLException;
+
+    List<Order> printUserCart(int id) throws SQLException;
 }
