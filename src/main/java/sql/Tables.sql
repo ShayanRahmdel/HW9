3c98506f632661baca76b0ecd2ca799ddd3e31be
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 10:28:42 +0330

    add shoetable

--- src/main/java/sql/Tables.sql
+++ src/main/java/sql/Tables.sql
@@ -23,8 +23,33 @@ CREATE TABLE ElectronicProducts(
 
 CREATE TABLE Orders(
     id SERIAL PRIMARY KEY ,
-    stock INT,
     user_id INT REFERENCES users(id),
-    elecproduct_id INT REFERENCES electronicproducts(id)
-
+    elecproduct_id INT REFERENCES electronicproducts(id),
+    shoesproduct_id INT REFERENCES shoeproduct(id)
 );
+
+CREATE TABLE Shoeproduct(
+    id SERIAL PRIMARY KEY ,
+    name VARCHAR(10),
+    prize NUMERIC,
+    stock INT
+);
+
+SELECT sum(prize) from electronicproducts
+join Orders on elecproduct_id=ElectronicProducts.id
+where user_id = 2  UNION ALL (SELECT sum(prize) from  shoeproduct
+ join orders on shoesproduct_id = Shoeproduct.id
+where user_id =2);
+
+SELECT SUM(total_price) AS total_sum
+FROM (
+         SELECT SUM(prize) AS total_price
+         FROM electronicproducts
+         WHERE id IN (SELECT elecproduct_id FROM Orders)
+
+         UNION ALL
+
+         SELECT SUM(prize) AS total_price
+         FROM shoeproduct
+         WHERE id IN (SELECT shoesproduct_id FROM Orders)
+     ) AS subquery;
