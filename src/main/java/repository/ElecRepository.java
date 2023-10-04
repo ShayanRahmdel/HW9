48ba223633a261c682543f1def81dae84d2ec900
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 10:25:12 +0330

    add two methods

--- src/main/java/repository/ElecRepository.java
+++ src/main/java/repository/ElecRepository.java
@@ -4,5 +4,11 @@ import base.repository.BaseRepository;
 import entity.Electronic;
 import entity.ElectronicProduct;
 
+import java.sql.SQLException;
+
 public interface ElecRepository extends BaseRepository<Integer, Electronic> {
+
+    int decreaseItem(int id) throws SQLException;
+
+    int checkStock(int id) throws SQLException;
 }
