0f8f5c549409b14ab30cc4efc366f8feddf8ffa8
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 19:50:11 +0330

    add  three methods

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -3,5 +3,18 @@ package service;
 import base.service.BaseService;
 import entity.Order;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface OrderService extends BaseService<Integer, Order> {
+
+
+
+    void setNull(int productID) throws SQLException;
+
+    void deleteByProductID(int id) throws SQLException;
+
+    boolean findProductId(int id) throws SQLException;
+
+    void printCart(int id) throws SQLException;
 }
