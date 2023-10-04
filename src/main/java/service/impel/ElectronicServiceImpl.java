cb7292409b7035ed21d2aa8b589f562634c94b4f
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 10:25:50 +0330

    add two methods decrease and checkStock

--- src/main/java/service/impel/ElectronicServiceImpl.java
+++ src/main/java/service/impel/ElectronicServiceImpl.java
@@ -5,8 +5,26 @@ import entity.Electronic;
 import repository.ElecRepository;
 import service.ElectronicService;
 
+import java.sql.SQLException;
+
 public class ElectronicServiceImpl extends BaseServiceImpl<Integer, Electronic, ElecRepository> implements ElectronicService {
+    private ElecRepository elecRepository;
+
     public ElectronicServiceImpl(ElecRepository repository) {
         super(repository);
+        this.elecRepository = repository;
     }
+
+    @Override
+    public void decreaseItem(int id) throws SQLException {
+        repository.decreaseItem(id);
+    }
+
+    @Override
+    public boolean checkStock(int id) throws SQLException {
+        if (elecRepository.checkStock(id) == 0) {
+            return true;
+        } else return false;
+    }
+
 }
