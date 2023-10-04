8c2cf7c74a8703bce18b31f67ed6ebb823b23ebf
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 19:50:16 +0330

    add  three methods

--- src/main/java/service/impel/OrderServiceImpl.java
+++ src/main/java/service/impel/OrderServiceImpl.java
@@ -5,8 +5,35 @@ import entity.Order;
 import repository.OrderRepository;
 import service.OrderService;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class OrderServiceImpl extends BaseServiceImpl<Integer, Order, OrderRepository> implements OrderService {
     public OrderServiceImpl(OrderRepository repository) {
         super(repository);
+    }
+
+
+
+    @Override
+    public void setNull(int productID) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteByProductID(int id) throws SQLException {
+        repository.deleteByProductID(id);
+    }
+
+    @Override
+    public boolean findProductId(int id) throws SQLException {
+        return id==repository.findProductID(id);
+    }
+
+    @Override
+    public void printCart(int id) throws SQLException {
+        List<Order> cart = repository.printUserCart(id);
+        System.out.println(cart);
+
     }
 }
