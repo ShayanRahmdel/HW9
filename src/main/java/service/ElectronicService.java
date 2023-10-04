c49103a57f1dc59582d34c2af268de23299bf72d
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 10:25:43 +0330

    add two methods decrease and checkStock

--- src/main/java/service/ElectronicService.java
+++ src/main/java/service/ElectronicService.java
@@ -3,5 +3,11 @@ package service;
 import base.service.BaseService;
 import entity.Electronic;
 
+import java.sql.SQLException;
+
 public interface ElectronicService extends BaseService<Integer, Electronic> {
+
+    void decreaseItem(int id) throws SQLException;
+
+    boolean checkStock(int id) throws SQLException;
 }
