b2627f325595d66424491536fead10430896e1ba
shayan rahmdel <shayan.rahmdel@gmail.com>
2023-10-04 10:27:07 +0330

    add method check id and stock

--- src/main/java/utility/ProductMenu.java
+++ src/main/java/utility/ProductMenu.java
@@ -2,6 +2,8 @@ package utility;
 
 import entity.Electronic;
 import entity.ElectronicProduct;
+import entity.Order;
+import entity.Shoe;
 
 import java.sql.SQLException;
 import java.util.InputMismatchException;
@@ -15,25 +17,25 @@ public class ProductMenu {
 
     public void StoreMenu() throws SQLException {
         System.out.println("1_Look All Product");
-        System.out.println("2_buy the Product");
+        System.out.println("2_order the Product");
         System.out.println("3_See Your Cart");
         int select = giveIntegerInput();
-        switch(select){
+        switch (select) {
             case 1 -> loadProduct();
-            case 2-> System.out.println("asd");
+            case 2 -> orderProduct();
             default -> System.out.println("sdasd");
         }
 
     }
-    private Integer giveIntegerInput(){
+
+    private Integer giveIntegerInput() {
         int i;
-        while (true){
+        while (true) {
             try {
-                i=input.nextInt();
+                i = input.nextInt();
                 input.nextLine();
                 return i;
-            }
-            catch (InputMismatchException e){
+            } catch (InputMismatchException e) {
                 input.nextLine();
                 System.out.println("Enter just number Please");
             }
@@ -41,7 +43,63 @@ public class ProductMenu {
     }
 
     private void loadProduct() throws SQLException {
-       List<Electronic> items =  applicationContext.getElectronicService().findAll();
+        System.out.println("================ Electronic Items ==================");
+        List<Electronic> items = applicationContext.getElectronicService().findAll();
         System.out.println(items);
+        System.out.println();
+        System.out.println("================Shoes Items ======================");
+        List<Shoe> shoes = applicationContext.getShoeService().findAll();
+        System.out.println(shoes);
+    }
+
+    private void orderProduct() throws SQLException {
+        System.out.print("Enter user id: ");
+        int userId = chechUserID();
+
+        System.out.print("Enter Electronic id: ");
+        int elecID = checkElectronicStock();
+        System.out.print("Enter Shoe id ");
+        int shoeID = checkShoesStock();
+
+        Order order = new Order(userId, elecID, shoeID);
+        applicationContext.getShoeService().decreaseItem(shoeID);
+        applicationContext.getElectronicService().decreaseItem(elecID);
+        applicationContext.getOrderService().save(order);
+    }
+
+    private int chechUserID() throws SQLException {
+        int userId = 0;
+        boolean flag = true;
+        while (flag) {
+            userId = giveIntegerInput();
+            if (applicationContext.getUserService().findUserId(userId)) {
+                flag = false;
+            } else System.out.println("Your ID is wrong please try again");
+        }
+        return userId;
+    }
+
+    private int checkShoesStock() throws SQLException {
+        int shoeID = 0;
+        boolean flag = true;
+        while (flag) {
+            shoeID = giveIntegerInput();
+            if (applicationContext.getShoeService().checkStock(shoeID)) {
+                System.out.println("Your stock is empty");
+            } else flag = false;
+        }
+        return shoeID;
+    }
+
+    private int checkElectronicStock() throws SQLException {
+        int elecID = 0;
+        boolean flag = true;
+        while (flag) {
+            elecID = giveIntegerInput();
+            if (applicationContext.getElectronicService().checkStock(elecID)) {
+                System.out.println("Your stock is empty");
+            } else flag = false;
+        }
+        return elecID;
     }
 }
